7024e1fdc914120736ff2e06ba38cc9a982268f7
Luzzi Charly <luzzi.charly@gmail.com>
2020-03-19 17:46:17 +0100

    Add vue/ionic

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Ionic from '@ionic/vue';
+import '@ionic/core/css/ionic.bundle.css';
+Vue.use(Ionic);
+
+
+let bus = new Vue
+Vue.prototype.$bus = bus
+
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Hello World</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Welcome To @ionic/vue</h1>
+      <img alt="Vue logo" src="../assets/logo.png">
+    </ion-content>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
+  export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
